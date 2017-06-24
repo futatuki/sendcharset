5e890120615877561515fd7ce852357d8956f4bc
FUTATSUKI Yasuhito <futatuki@yf.bsdclub.org>
2017-06-25 04:24:32 +0900

    Add checkbox to set use_base64 option (it will be move section to set later)

--- sendcharset.php
+++ sendcharset.php
@@ -20,21 +20,27 @@ class sendcharset extends rcube_plugin
    */
   function show_option($attrib)
   {
-    global $RCMAIL;
+    $rcmail = rcmail::get_instance();
 
     if ($attrib['section'] == 'compose') {
       $field_id = 'rcmfd_sendcharset';
       $selected = $this->get_charset();
-      $input = $RCMAIL->output->charset_selector(array('name'=>'_sendcharset',
+      $input = $rcmail->output->charset_selector(array('name'=>'_sendcharset',
 						       'id'=>$field_id,
 						       'selected'=>$selected));
       $attrib['blocks']['main']['options']['sendcharset'] =
-	array( 'title'=>html::label($field_id, Q(rcube_label('charset'))),
+	array( 'title'=>html::label($field_id, $this->gettext(rcube_label('charset'))),
 	       'content'=>$input);
-      return $attrib;
-    } else {
-      return null;
+
+      // add checkbox for use_base64
+      $field_id = 'rcmfd_use_base64';
+      $checkbox = new html_checkbox(array('name' => '_use_base64', 'id' => $field_id, 'value' => 1));
+      $attrib['blocks']['main']['options']['use_base64'] = array(
+		'title'   => $this->gettext('usebase64'),
+		'content' => $checkbox->show(intval($rcmail->config->get('use_base64', false)))
+      );
     }
+    return $attrib;
   }
 
   /**
@@ -46,6 +52,7 @@ class sendcharset extends rcube_plugin
 	$attrib['prefs']['sendcharset'] =
 	  get_input_value('_sendcharset', RCUBE_INPUT_POST);
       }
+      $attrib['prefs']['use_base64'] = rcube_utils::get_input_value('_use_base64', rcube_utils::INPUT_POST) ? true : false;
     }
     return $attrib;
   }
@@ -65,8 +72,8 @@ class sendcharset extends rcube_plugin
    */
   function tweak_encoding($params)
   {
-    global $RCMAIL, $OUTPUT;
-    $config = $RCMAIL->config->all();
+    $rcmail = rcmail::get_instance();
+    $config = $rcmail->config->all();
     $MAIL_MIME = $params['message'];
     /* this is abuse ... */
     $message_charset = $MAIL_MIME->getParam('html_charset');
@@ -75,17 +82,17 @@ class sendcharset extends rcube_plugin
       $MAIL_MIME->setParam('html_encoding', 'base64');
       $MAIL_MIME->setParam('head_encoding', 'base64');
       if ($MAIL_MIME->getParam('text_encoding') == 'quoted-printable') {
-        $MAIL_MIME->setParam('text_encoding', 'base64');
+	$MAIL_MIME->setParam('text_encoding', 'base64');
       }
     }
-    if (strtolower($message_charset)  == 'iso-2022-jp') {
+    if (preg_match('/iso-2022/i', $message_charset)) {
       $MAIL_MIME->setParam('head_encoding', 'base64');
       $MAIL_MIME->setParam('text_encoding', '7bit');
       if (preg_match('/format=flowed/', $MAIL_MIME->getParam('text_charset'))){
-        $MAIL_MIME->setParam('text_charset', "ISO-2022-JP;\r\n format=flowed");
+	$MAIL_MIME->setParam('text_charset', $message_charset . ";\r\n format=flowed");
       }
       else {
-        $MAIL_MIME->setParam('text_charset', "ISO-2022-JP");
+	$MAIL_MIME->setParam('text_charset', $message_charset);
       }
     }
     return $params;
@@ -96,8 +103,9 @@ class sendcharset extends rcube_plugin
    * If it is not set, return GUI coding system.
    */
   private function get_charset() {
-    global $RCMAIL, $OUTPUT;
-    $config = $RCMAIL->config->all();
+    global $OUTPUT;
+    $rcmail = rcmail::get_instance();
+    $config = $rcmail->config->all();
 
     return isset($config['sendcharset']) ?
       $config['sendcharset'] : $OUTPUT->get_charset();
