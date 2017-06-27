79df8889209a39a4d49d9721655204382401f843
FUTATSUKI Yasuhito <futatuki@yf.bsdclub.org>
2017-06-28 04:05:53 +0900

    remove code to modify encodings

--- sendcharset.php
+++ sendcharset.php
@@ -19,7 +19,6 @@ class sendcharset extends rcube_plugin
     }
     else { /* if ($this->rc->task == 'mail') */
       $this->add_hook('template_object_composebody', array($this, 'append'));
-      $this->add_hook('message_ready', array($this, 'tweak_encoding'));
     }
   }
 
@@ -32,26 +31,17 @@ class sendcharset extends rcube_plugin
       return $attrib;
     }
     $dont_override = $this->rc->config->get('dont_override', array());
-    $this->add_texts('localization/', true);
 
     if (!in_array('sendcharset', $dont_override)) {
+      $this->add_texts('localization/', true);
       $field_id = 'rcmfd_sendcharset';
       $selected = $this->get_charset();
       $input = $this->rc->output->charset_selector(array('name'=>'_sendcharset',
 						       'id'=>$field_id,
 						       'selected'=>$selected));
       $attrib['blocks']['main']['options']['sendcharset'] =
-	array( 'title'=> html::label($field_id, $this->gettext('sendcharset')),
+	array( 'title'=>html::label($field_id, $this->gettext('sendcharset')),
 	       'content'=>$input);
-    }
-    if (!in_array('use_base64', $dont_override)) {
-      // add checkbox for use_base64
-      $field_id = 'rcmfd_use_base64';
-      $checkbox = new html_checkbox(array('name' => '_use_base64', 'id' => $field_id, 'value' => 1));
-      $attrib['blocks']['advanced']['options']['use_base64'] = array(
-		'title'   => html::label($field_id, $this->gettext('usebase64')),
-		'content' => $checkbox->show(intval($this->rc->config->get('use_base64', false)))
-      );
     }
     return $attrib;
   }
@@ -70,9 +60,6 @@ class sendcharset extends rcube_plugin
 	  get_input_value('_sendcharset', RCUBE_INPUT_POST);
       }
     }
-    if (!in_array('use_base64', $dont_override)) {
-      $attrib['prefs']['use_base64'] = rcube_utils::get_input_value('_use_base64', rcube_utils::INPUT_POST) ? true : false;
-    }
     return $attrib;
   }
 
@@ -84,35 +71,6 @@ class sendcharset extends rcube_plugin
     $input = new html_inputfield(array('type'=>'hidden', 'name'=>'_charset'));
     $attrib['content'] .= "\n".$input->show($this->get_charset());
     return $attrib;
-  }
-
-  /**
-   * hack the encodings of the message before sending it
-   */
-  function tweak_encoding($params)
-  {
-    $config = $this->rc->config->all();
-    $MAIL_MIME = $params['message'];
-    /* this is abuse ... */
-    $message_charset = $MAIL_MIME->getParam('html_charset');
-    if (isset($config['use_base64']) and $config['use_base64']) {
-      $MAIL_MIME->setParam('html_encoding', 'base64');
-      $MAIL_MIME->setParam('head_encoding', 'base64');
-      if ($MAIL_MIME->getParam('text_encoding') == 'quoted-printable') {
-	$MAIL_MIME->setParam('text_encoding', 'base64');
-      }
-    }
-    if (preg_match('/iso-2022/i', $message_charset)) {
-      $MAIL_MIME->setParam('head_encoding', 'base64');
-      $MAIL_MIME->setParam('text_encoding', '7bit');
-      if (preg_match('/format=flowed/', $MAIL_MIME->getParam('text_charset'))){
-	$MAIL_MIME->setParam('text_charset', $message_charset . ";\r\n format=flowed");
-      }
-      else {
-	$MAIL_MIME->setParam('text_charset', $message_charset);
-      }
-    }
-    return $params;
   }
 
   /**
